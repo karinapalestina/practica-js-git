12dfbe2b763da9de817f521401b4483710acb79a
karina fragoso palestina <UCX_FAPK_LSCA_24@UCALMECAC.EDU.MX>
2026-07-08 13:30:45 -0600

    Ejercicio 01: ficha de datos personales

---
+++ ejercicio01.js
@@ -0,0 +1,15 @@
+
+let nombreCompleto = "Karina Fragoso Palestina";
+let edad = 35;
+let promedio = 8.4;
+
+const matricula = "UCX24WS88";
+const carrera = "Licenciatura en Sistemas Computacionales Administrativos";
+
+console.log(` === FICHA DEL ALUMNO ===
+Nombre: ${nombreCompleto}
+Edad: ${edad}
+Matrícula: ${matricula}
+Carrera: ${carrera}
+Promedio: ${promedio}
+`);
