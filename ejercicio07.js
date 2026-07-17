17ff2547606db02816600d1d3afacc7c27d27ab5
karina palestina <palestina152326@gmail.com>
2026-07-17 01:20:41 -0600

    Ejercicio 07: comparaciones y operadores de igualdad

---
+++ ejercicio07.js
@@ -0,0 +1,7 @@
+
+console.log("10 == \"10\":", 10 == "10");
+console.log("10 === \"10\":", 10 === "10");
+console.log("0 == false:", 0 == false);
+console.log("0 === false:", 0 === false);
+console.log("\"abc\" < \"abd\":", "abc" < "abd");
+console.log("5 !== 5.0:", 5 !== 5.0);
