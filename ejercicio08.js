2f0d112c6a547722dfd1190af4730c51fc440799
karina palestina <palestina152326@gmail.com>
2026-07-17 01:34:07 -0600

    Ejercicio08: reglamento de beca

---
+++ ejercicio08.js
@@ -0,0 +1,11 @@
+
+let promedio = 9.5;
+let materiasReprobadas = 0;
+let esDeportista = false;
+
+// Expresión lógica
+let obtieneBeca = (promedio >= 9.0 && materiasReprobadas === 0) || esDeportista;
+
+// Mostrar resultado
+console.log(`¿Obtiene beca?: ${obtieneBeca}`);
+
